f3f4bd2f5ebf1faf0109eca62ee4f4b0b34afcff
fmaya02 <facundojulianmaya@gmail.com>
2023-10-04 15:47:59 -0300

    Buildeo SNAPSHOT y config

--- src/main/java/com/facu/restfake/repositories/BaseRepository.java
+++ src/main/java/com/facu/restfake/repositories/BaseRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
-// Para que se instancie
+
 @NoRepositoryBean
-// public interface BaseRepository <E,ID>{
-//public interface BaseRepository <E extends Base,ID extends Serializable>{
+
 public interface BaseRepository <E extends Base,ID extends Serializable> extends JpaRepository <E,ID> {
 }
